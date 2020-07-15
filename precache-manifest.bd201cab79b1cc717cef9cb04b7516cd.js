ba35591a4f791f8e3e890cf8ebd413e9d211df58
unknown <pranayburra66@gmail.com>
2020-07-15 13:40:30 +0530

    Updates

---
+++ precache-manifest.bd201cab79b1cc717cef9cb04b7516cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762372d2793e3509a411cf9d7787f23c",
+    "url": "/mekvahanintern/index.html"
+  },
+  {
+    "revision": "8745d25bf08893533ae2",
+    "url": "/mekvahanintern/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "ffa9b8472dc1350d1882",
+    "url": "/mekvahanintern/static/css/main.05a7e5c6.chunk.css"
+  },
+  {
+    "revision": "8745d25bf08893533ae2",
+    "url": "/mekvahanintern/static/js/2.169a8d3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mekvahanintern/static/js/2.169a8d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa9b8472dc1350d1882",
+    "url": "/mekvahanintern/static/js/main.d0dd7561.chunk.js"
+  },
+  {
+    "revision": "646c01be2ec961a9d331",
+    "url": "/mekvahanintern/static/js/runtime-main.fd0e555c.js"
+  },
+  {
+    "revision": "aa52fcc749119df4bf539cee73179a14",
+    "url": "/mekvahanintern/static/media/bg.aa52fcc7.png"
+  },
+  {
+    "revision": "55f9ddefeb4cb0c19d89608c7ebc32b3",
+    "url": "/mekvahanintern/static/media/contact.55f9ddef.png"
+  },
+  {
+    "revision": "10408929cd8215e35f31574ff9998f41",
+    "url": "/mekvahanintern/static/media/logo.10408929.svg"
+  },
+  {
+    "revision": "9979e7e24832d0466ffdb86a39ec7560",
+    "url": "/mekvahanintern/static/media/oops.9979e7e2.svg"
+  }
+]);
